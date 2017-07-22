2949c9fc72c9047360d0d5eea1708cc3298a89bf
xueyang <1120880710@qq.com>
2017-07-22 21:41:25 +0800

    新功能增加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+    DashboardComponent
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
